c7105dcf0229b5763aca445e45d09eeaa2ee53ed
Chirag Jain <chiragjain7300@gmail.com>
2024-05-19 20:18:22 +0530

    video and user model creation

---
+++ src/models/videos.models.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectID,
+      ref: "User",
+    },
+    videoFile: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
